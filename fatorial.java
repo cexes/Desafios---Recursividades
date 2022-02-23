dfa98e37169246057b11abff251eea4255b63b8a
César Henrique <80805964+cexes@users.noreply.github.com>
2022-02-23 12:27:37 -0300

    Update fatorial.java

--- fatorial.java
+++ fatorial.java
@@ -4,11 +4,11 @@ public class fatorial {
 
      public int finabonacci(int n) {
     	 
-    	   if(n == 0) {
-    		   return 0;
+    	   if(n <2) {
+    		   return n;
     	   }
     	   
-    	   return finabonacci(n - 1	) + finabonacci(n -2);
+    	   return finabonacci(n - 1) + finabonacci(n -2);
      }
 
 
