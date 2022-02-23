bc50812504d9091552c0fbd060fdde5a6bd01579
César Henrique <80805964+cexes@users.noreply.github.com>
2022-02-23 12:23:48 -0300

    Create App.java

---
+++ App.java
@@ -0,0 +1,14 @@
+package estrutura_daddos;
+
+public class es {
+
+	public static void main(String[] args) {
+	
+		
+		
+	  fatorial fator = new fatorial();
+	  System.out.println(fator.finabonacci(1));
+		
+		
+	}
+ }
