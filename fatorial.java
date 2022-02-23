114c0b3b600fedc18a817e16c59020fe1bd8f69e
César Henrique <80805964+cexes@users.noreply.github.com>
2022-02-23 12:24:24 -0300

    Create fatorial.java

---
+++ fatorial.java
@@ -0,0 +1,17 @@
+public class fatorial {
+
+
+
+     public int finabonacci(int n) {
+    	 
+    	   if(n == 0) {
+    		   return 0;
+    	   }
+    	   
+    	   return finabonacci(n - 1	) + finabonacci(n -2);
+     }
+
+
+
+
+}
